5632e7713307bb6015954f1eeb0759526aa23174
Callasaurus <c.sullivandrage@outlook.com>
2022-11-29 14:06:26 +0000

    pushing some minor validation changes

--- bc13_w9_project-frontend-codebusters-front/src/components/Input/Input.js
+++ bc13_w9_project-frontend-codebusters-front/src/components/Input/Input.js
@@ -20,77 +20,91 @@ const {
 if (props.language === 'englishDefinitions') {
     
     return (
-        <div className="form-items">
+    <div className="form-items">
 
-            <div className="exitButtonDiv">
-                <button className="exitButton" onClick={props.visibility}>X</button>
-            </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                    <label>Title</label>
-                    <input 
-                    type="text" 
-                    name="title" 
-                    {...register("title", {
-                        required: props.editOrAdd,
-                        minLength: 3
-                    })} 
-                    placeholder="Title of the resource">
-                    </input>
-                    {errors.title && errors.title.type === "required" && (
-                        <p>Title must not be blank</p>
-                    )}
-                    {errors.title && errors.title.type === "minLength" && (
-                        <p>Title must have at least 4 characters</p>
-                    )}
-                    <label>Definition</label>
-                    <input 
-                    type="text" 
-                    name="definition" 
-                    {...register("definition", {
-                        required: props.editOrAdd,
-                        minLength: 20
-                    })} 
-                    placeholder="Definition of the resouce topic">
-                    </input>
-                    {errors.definition && errors.definition.type === "required" && (
-                        <p>Definition must not be blank</p>
-                    )}
-                    {errors.definition && errors.definition.type === "minLength" && (
-                        <p>Definition must have at least 20 characters</p>
-                    )}
-                    <label>Example</label>
-                    <input 
-                    type="text" 
-                    name="example" {...register("example", {
-                    required: props.editOrAdd,
-                    minLength: 10
-                    })} 
-                    placeholder="Example (image url)"></input>
-                    <label>Links</label>
-                    <input 
-                    type="text" 
-                    name="links" {...register("links", {
-                    required: props.editOrAdd,
-                    minLength: 10
-                    })} 
-                    placeholder="Links (url of helpful website(s))"></input>
-                    <label>Week</label>
-                    <input 
-                    type="number"
-                    min="0"
-                    max="16"
-                    name="week" {...register("week", {
-                        required: props.editOrAdd
-                    })} 
-                    placeholder="Week content covered (enter a number)">
-                    </input>
-                    {errors.week && errors.week.type === "required" && (
-                        <p>Week must not be blank</p>
-                    )}
-                <button type="submit" className="addButton">Add</button>
-            </form>
+        <div className="exitButtonDiv">
+            <button className="exitButton" onClick={props.visibility}>X</button>
         </div>
-    )
+        <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Title</label>
+                <input 
+                type="text" 
+                name="title" 
+                {...register("title", {
+                    required: props.editOrAdd,
+                    minLength: 3
+                })} 
+                placeholder="Title of the resource">
+                </input>
+                {errors.title && errors.title.type === "required" && (
+                    <p className='error'>Title must not be blank</p>
+                )}
+                {errors.title && errors.title.type === "minLength" && (
+                    <p className='error'>Title must have at least 4 characters</p>
+                )}
+                <label>Definition</label>
+                <input 
+                type="text" 
+                name="definition" 
+                {...register("definition", {
+                    required: props.editOrAdd,
+                    minLength: 20
+                })} 
+                placeholder="Definition of the resouce topic">
+                </input>
+                {errors.definition && errors.definition.type === "required" && (
+                    <p className='error'>Definition must not be blank</p>
+                )}
+                {errors.definition && errors.definition.type === "minLength" && (
+                    <p className='error'>Definition must have at least 20 characters</p>
+                )}
+                <label>Example</label>
+                <input 
+                type="text" 
+                name="example" {...register("example", {
+                    required: props.editOrAdd,
+                    minLength: 10
+                })} 
+                placeholder="Example (image url)">
+                </input>
+                {errors.example && errors.example.type === "required" && (
+                    <p className='error'>Example must not be blank</p>
+                )}
+                {errors.example && errors.example.type === "minLength" && (
+                    <p className='error'>Example must have at least 10 characters</p>
+                )}
+                <label>Links</label>
+                <input 
+                type="text" 
+                name="links" {...register("links", {
+                    required: props.editOrAdd,
+                    minLength: 10
+                })} 
+                placeholder="Links (url of helpful website(s))">
+                </input>
+                {errors.links && errors.links.type === "required" && (
+                    <p className='error'>Link must not be blank</p>
+                )}
+                {errors.links && errors.links.type === "minLength" && (
+                    <p className='error'>Link must have at least 10 characters</p>
+                )}
+                <label>Week</label>
+                <input 
+                type="number"
+                min="0"
+                max="16"
+                name="week" {...register("week", {
+                    required: props.editOrAdd
+                })} 
+                placeholder="Week content covered (enter a number)">
+                </input>
+                {errors.week && errors.week.type === "required" && (
+                    <p className='error'>Week must not be blank</p>
+                )}
+            <button type="submit" className="addButton">Add</button>
+        </form>
+    </div>
+)
 
 } else {
     
@@ -112,10 +126,10 @@ if (props.language === 'englishDefinitions') {
                 placeholder="Title of the resource">
                 </input>
                 {errors.englishtitle && errors.englishtitle.type === "required" && (
-                    <p>Title must not be blank</p>
+                    <p className='error'>Title must not be blank</p>
                 )}
                 {errors.englishtitle && errors.englishtitle.type === "minLength" && (
-                    <p>Title must have at least 4 characters</p>
+                    <p className='error'>Title must have at least 4 characters</p>
                 )}
                 <label>Title</label>
                 <input 
@@ -128,10 +142,10 @@ if (props.language === 'englishDefinitions') {
                 placeholder="Title of the resource">
                 </input>
                 {errors.title && errors.title.type === "required" && (
-                    <p>Title must not be blank</p>
+                    <p className='error'>Title must not be blank</p>
                 )}
                 {errors.title && errors.title.type === "minLength" && (
-                    <p>Title must have at least 4 characters</p>
+                    <p className='error'>Title must have at least 4 characters</p>
                 )}
                 <label>Definition</label>
                 <input 
@@ -144,10 +158,10 @@ if (props.language === 'englishDefinitions') {
                 placeholder="Definition of the resouce topic">
                 </input>
                 {errors.definition && errors.definition.type === "required" && (
-                    <p>Definition must not be blank</p>
+                    <p className='error'>Definition must not be blank</p>
                 )}
                 {errors.definition && errors.definition.type === "minLength" && (
-                    <p>Definition must have at least 20 characters</p>
+                    <p className='error'>Definition must have at least 20 characters</p>
                 )}
                 <label>Example</label>
                 <input 
@@ -156,7 +170,14 @@ if (props.language === 'englishDefinitions') {
                     required: props.editOrAdd,
                     minLength: 10
                 })} 
-                placeholder="Example (image url)"></input>
+                placeholder="Example (image url)">
+                </input>
+                {errors.example && errors.example.type === "required" && (
+                    <p className='error'>Example must not be blank</p>
+                )}
+                {errors.example && errors.example.type === "minLength" && (
+                    <p className='error'>Example must have at least 10 characters</p>
+                )}
                 <label>Links</label>
                 <input 
                 type="text" 
@@ -164,7 +185,14 @@ if (props.language === 'englishDefinitions') {
                     required: props.editOrAdd,
                     minLength: 10
                 })} 
-                placeholder="Links (url of helpful website(s))"></input>
+                placeholder="Links (url of helpful website(s))">
+                </input>
+                {errors.links && errors.links.type === "required" && (
+                    <p className='error'>Link must not be blank</p>
+                )}
+                {errors.links && errors.links.type === "minLength" && (
+                    <p className='error'>Link must have at least 10 characters</p>
+                )}
                 <label>Week</label>
                 <input 
                 type="number"
@@ -176,7 +204,7 @@ if (props.language === 'englishDefinitions') {
                 placeholder="Week content covered (enter a number)">
                 </input>
                 {errors.week && errors.week.type === "required" && (
-                    <p>Week must not be blank</p>
+                    <p className='error'>Week must not be blank</p>
                 )}
             <button type="submit" className="addButton">Add</button>
         </form>
