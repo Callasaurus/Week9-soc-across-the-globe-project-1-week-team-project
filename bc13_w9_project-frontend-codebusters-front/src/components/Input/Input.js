dff511202a26097ce150e51438f9b19a629ed860
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-11-29 11:29:01 +0000

    form hook

--- bc13_w9_project-frontend-codebusters-front/src/components/Input/Input.js
+++ bc13_w9_project-frontend-codebusters-front/src/components/Input/Input.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Input.css'
 import { useState } from 'react'
+import { useForm } from "react-hook-form";
 
 export function Input(props) {
 
@@ -76,6 +77,18 @@ function Validate(value) {
     return false
 }
 
+
+const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    props.handleNewObject(data);
+    props.visibility()
+  };
+
 if (props.language === 'englishDefinitions') {
     return (
         <div className="form-items">
@@ -83,15 +96,19 @@ if (props.language === 'englishDefinitions') {
             <div className="exitButtonDiv">
                 <button className="exitButton" onClick={props.visibility}>X</button>
             </div>
-            
-            <input placeholder="Title of the resource" onChange={handleTitle} value={title}></input>
-            <input placeholder="Definition of the resouce topic" onChange={handleDefinition} value={definition}></input>
-            <input placeholder="Example (image url)" onChange={handleExample} value={example}></input>
-            <input placeholder="Links (url of helpful website(s))" onChange={handleLinks} value={links}></input>
-            <input placeholder="Week content covered (enter a number)" onChange={handleWeek} value={week}></input>
-
-            <button className="addButton" onClick={createOrEditObject}>Add</button>
-
+            <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>Title</label>
+                    <input type="text" name="title" {...register("title")} placeholder="Title of the resource" value={title}></input>
+                    <label>Definition</label>
+                    <input type="text" name="definition" {...register("definition")} placeholder="Definition of the resouce topic" value={definition}></input>
+                    <label>Example</label>
+                    <input type="text" name="example" {...register("example")} placeholder="Example (image url)" value={example}></input>
+                    <label>Links</label>
+                    <input type="text" name="links" {...register("links")} placeholder="Links (url of helpful website(s))" value={links}></input>
+                    <label>Week</label>
+                    <input type="text" name="week" {...register("week")} placeholder="Week content covered (enter a number)" value={week}></input>
+                <button type="submit" className="addButton">Add</button>
+            </form>
         </div>
     )
 } else {
