5d132021ae78ba4be2212c84f49bc45e911cecdc
L-McHugh <l.mchugh1@outlook.com>
2022-11-30 12:10:43 +0000

    start page testing

--- bc13_w9_project-frontend-codebusters-front/src/components/StartPage/StartPage.js
+++ bc13_w9_project-frontend-codebusters-front/src/components/StartPage/StartPage.js
@@ -8,10 +8,10 @@ export function StartPage(props) {
                 <img className="animation-logo" src={logos} alt="logo" />
             </div>
 
-            <h3 className="chooseYourLanguage">Choose Your Language</h3>
+            <h3 id="title" className="chooseYourLanguage">Choose Your Language</h3>
 
             <div className="langButtonDiv">
-                <button className="langButtonEnglish" onClick={props.changeEnglish}></button>
+                <button id="langButtonEnglish" className="langButtonEnglish" onClick={props.changeEnglish}></button>
                 <button className="langButtonSpanish" onClick={props.changeSpanish}></button>
                 <button className="langButtonFrench" onClick={props.changeFrench}></button>
                 <button className="langButtonGerman" onClick={props.changeGerman}></button>
---
+++ bc13_w9_project-frontend-codebusters-front/src/components/StartPage/StartPage.test.js
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import {test, expect} from '@jest/globals'
+import React from 'react'
+import '@testing-library/jest-dom'
+import {StartPage} from './StartPage';
+
+
+
+test('renders logo', () => {
+    //ARRANGE
+  render(<StartPage />);
+ 
+    //ACT
+    const logo = screen.getByAltText('logo')
+
+    //ASSERT
+    expect(logo).toBeInTheDocument()
+});
+
+
+test('renders title', () => {
+    //ARRANGE
+  render(<StartPage />);
+
+    //ACT
+    const title = screen.getByText('Choose Your Language', {exact:false});
+
+    //ASSERT
+    expect(title).toBeInTheDocument();
+   
+});
+
+
+test('renders flags', () => {
+    //ARRANGE
+  render(<StartPage />);
+ 
+    //ACT
+    const flagElement = screen.getAllByRole('button');
+
+    //ASSERT
+    expect(flagElement).toHaveLength(4);
+});
+
+test('buttons click', () => {
+    //ARRANGE
+    render(<StartPage />);
+    
+    //ACT
+    const buttonClick = screen.getAllByRole('button');
+    buttonClick[0].click();
+    buttonClick[1].click();
+    buttonClick[2].click();
+    buttonClick[3].click();
+    
+    //ASSERT
+    expect(buttonClick).toHaveLength(4);
+});
